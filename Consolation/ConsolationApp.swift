e8bd94c055b0c0d8750f28ccd86a8248bad3c720
James Ranson <james@ranson.org>
2026-04-12 02:17:53 -0600

    Initial Commit

---
+++ Consolation/ConsolationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsolationApp.swift
+//  Consolation
+//
+//  Created by James Ranson on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct ConsolationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConsolationTests/ConsolationTests.swift
@@ -0,0 +1,18 @@
+//
+//  ConsolationTests.swift
+//  ConsolationTests
+//
+//  Created by James Ranson on 4/12/26.
+//
+
+import Testing
+
+struct ConsolationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
